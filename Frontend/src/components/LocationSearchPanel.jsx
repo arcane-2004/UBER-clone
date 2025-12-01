c2271d094896ace2a8a556985deac0b8f1f24c1a
Sumit Kumar Panika <sumitkp153@gmail.com>
2025-12-01 18:29:45 +0530

    Update LocationSearchPanel.jsx

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-const LocationSearchPanel = ({ suggestions, setPickup, setDestination, activeField, findTrip }) => {
+const LocationSearchPanel = ({ suggestions, setPickup, setPickupId, setDestination, setDestinationId, activeField, findTrip }) => {
 
     const handleSuggestionClick = (suggestion) => {
-        if (activeField === 'pickup') {
-            setPickup(suggestion)
+        if (activeField === 'pickup') {     
+            setPickup(suggestion.name)
+            setPickupId(suggestion.mapbox_id)
         } else if (activeField === 'destination') {
-            setDestination(suggestion)
+            setDestination(suggestion.name)
+            setDestinationId(suggestion.mapbox_id)
         }
         // setVehiclePanel(true)
         // setPanelOpen(false)
@@ -24,12 +26,12 @@ const LocationSearchPanel = ({ suggestions, setPickup, setDestination, activeFie
                 {
                     suggestions.map((elem, idx) => (
 
-                        <div key={idx} onClick={() => handleSuggestionClick(elem.description)} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
+                        <div key={idx} onClick={() => handleSuggestionClick(elem)} className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start'>
                             <div>
                                 <h2 className='bg-[#eee] h-8 w-8 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
                             </div>
                             <div>
-                                <h4 className='font-medium'>{elem.description}</h4>
+                                <h4 className='font-medium'>{elem.name}</h4>
                             </div>
 
                         </div>
